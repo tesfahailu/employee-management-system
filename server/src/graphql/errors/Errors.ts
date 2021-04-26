10210c901bfa488b81f315435b3f4c5686b1cc56
Tesfa Hailu <>
2021-04-26 09:57:56 -0400

    update: include files changed when updating resolvers: role, permission, and resource

---
+++ server/src/graphql/errors/Errors.ts
@@ -0,0 +1,19 @@
+export enum ErrorCode {
+  BAD_USER_INPUT = 'User input error',
+  VALIDATION_ERROR = 'Validation error',
+}
+
+export enum ErrorMessage {
+  UNKNOWN = 'unknown error occurred.',
+  NAME_ALREADY_EXIST = 'name already exist',
+
+  ROLE_ARG_ERROR = 'provide a role id or name.',
+  ROLE_NOT_FOUND = 'role not found. check role id or name.',
+
+  PERMISSION_ARG_ERROR = 'provide either a valid permission id or a valid role id and resource id',
+  PERMISSION_NOT_FOUND = 'permission not found. check permission id.',
+  PERMISSION_INVALID_RESOURCE_ID = 'permission does not have a valid resource id.',
+
+  RESOURCE_ARG_ERROR = 'provide a resource id or name',
+  RESOURCE_NOT_FOUND = 'resource not found. check resource id or name.',
+}
