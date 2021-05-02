82f99c8767c84523ca2a7d6be51a875aaba02ad0
tesfahailu <40552713+tesfahailu@users.noreply.github.com>
2021-05-02 14:22:10 -0400

    create: client scaffolding, webpack, babel, and React (#15)
    Co-authored-by: Tesfa Hailu <>

---
+++ client/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+  },
+  entry: { index: './src/index.tsx' },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    chunkFilename: '[id].[chunkhash].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+      filename: 'index.html',
+    }),
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public', 'index.html'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts[x]?$/,
+        use: ['babel-loader', 'ts-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
