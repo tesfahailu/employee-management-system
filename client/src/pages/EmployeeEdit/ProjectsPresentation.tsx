9c29490cb4a55af26dffd07e4d2a6800f0382e14
Tesfa Hailu <>
2021-05-18 09:30:43 -0400

    create: edit page for employee projects

---
+++ client/src/pages/EmployeeEdit/ProjectsPresentation.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ProjectType } from '../../types/types';
+
+export const ProjectsPresentation = ({
+  projects,
+}: {
+  projects: Array<ProjectType>;
+}) => {
+  return <div>Cool</div>;
+};
