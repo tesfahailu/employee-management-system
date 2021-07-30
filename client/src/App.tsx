fe337cfd36e1bffaac417c6d38348b52b924e9a6
Tesfa Hailu <>
2021-07-29 22:43:51 -0400

    update: color picker and dark/light mode

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { ToggleThemeContext, useDarkMode } from './components/Theme';
+import { ThemeProvider } from './components/Theme';
 import { setAccessToken } from './services/session/accessToken';
 import { GeneralRoutes } from './routes/Routes';
 import './App.css';
-import { createTheme, ThemeProvider } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
 
 export const App: React.FC = ({}) => {
-  const [theme, toggleDarkMode] = useDarkMode();
-  const themeConfig = createTheme(theme);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -24,11 +21,9 @@ export const App: React.FC = ({}) => {
 
   if (loading) return <div>loading...</div>;
   return (
-    <ThemeProvider theme={themeConfig}>
-      <ToggleThemeContext.Provider value={toggleDarkMode}>
-        <CssBaseline />
-        <GeneralRoutes />
-      </ToggleThemeContext.Provider>
+    <ThemeProvider>
+      <CssBaseline />
+      <GeneralRoutes />
     </ThemeProvider>
   );
 };
