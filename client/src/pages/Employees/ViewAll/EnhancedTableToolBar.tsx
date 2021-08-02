06c47f1cef882c31412da664a9722f323168a8d4
tesfahailu <40552713+tesfahailu@users.noreply.github.com>
2021-08-02 17:51:36 -0400

    update: using material-ui table w/ searching, downloading csv, and table sorting (#37)

---
+++ client/src/pages/Employees/ViewAll/EnhancedTableToolBar.tsx
@@ -0,0 +1,149 @@
+import React, { useState } from 'react';
+import { IconButton, InputBase, Stack } from '@material-ui/core';
+import { alpha, styled } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Tooltip from '@material-ui/core/Tooltip';
+import { Delete as DeleteIcon } from '@material-ui/icons';
+import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
+import { CSVLink, CSVDownload } from 'react-csv';
+import { Rows } from './testData';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+function SearchBox({ toggleSearchBox }: { toggleSearchBox: () => void }) {
+  return (
+    <Search onBlur={toggleSearchBox}>
+      <SearchIconWrapper>
+        <SearchIcon />
+      </SearchIconWrapper>
+      <StyledInputBase
+        placeholder="Search…"
+        inputProps={{ 'aria-label': 'search' }}
+        autoFocus
+      />
+    </Search>
+  );
+}
+
+interface EnhancedTableToolbarProps {
+  numSelected: number;
+  rowsData: Array<Rows>;
+}
+
+export const EnhancedTableToolbar = (props: EnhancedTableToolbarProps) => {
+  const { numSelected, rowsData } = props;
+  const [isSearch, setIsSearch] = useState(false);
+
+  const toggleSearchBox = () => {
+    setIsSearch((prevValue) => {
+      return !prevValue;
+    });
+  };
+
+  return (
+    <Toolbar
+      sx={{
+        pl: { sm: 2 },
+        pr: { xs: 1, sm: 1 },
+        ...(numSelected > 0 && {
+          bgcolor: (theme) =>
+            alpha(
+              theme.palette.primary.main,
+              theme.palette.action.activatedOpacity,
+            ),
+        }),
+      }}
+    >
+      {numSelected > 0 ? (
+        <Typography
+          sx={{ flex: '1 1 100%' }}
+          color="inherit"
+          variant="subtitle1"
+          component="div"
+        >
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <Typography
+          sx={{ flex: '1 1 100%' }}
+          variant="h6"
+          id="tableTitle"
+          component="div"
+        >
+          Employees
+        </Typography>
+      )}
+      {numSelected > 0 ? (
+        <Tooltip title="Delete">
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      ) : (
+        <Stack direction="row" spacing={0.8} justifyContent="flex-start">
+          {isSearch ? (
+            <SearchBox toggleSearchBox={toggleSearchBox} />
+          ) : (
+            <Tooltip title="Search">
+              <IconButton>
+                <SearchIcon onClick={toggleSearchBox} />
+              </IconButton>
+            </Tooltip>
+          )}
+          <CSVLink
+            data={rowsData.map(({ id, ...rest }) => rest)}
+            id="contained-button-csv"
+            filename="employees.csv"
+          >
+            <Tooltip title="Download CSV">
+              <IconButton>
+                <CloudDownloadIcon />
+              </IconButton>
+            </Tooltip>
+          </CSVLink>
+        </Stack>
+      )}
+    </Toolbar>
+  );
+};
--- client/src/pages/Employees/ViewAll/ViewAllPresentation.tsx
+++ client/src/pages/Employees/ViewAll/ViewAllPresentation.tsx
@@ -1,18 +1,13 @@
 import React, { Fragment } from 'react';
-import { Button, Grid, IconButton, Typography } from '@material-ui/core';
+import { Button, Grid, Typography } from '@material-ui/core';
 import { createStyles, makeStyles } from '@material-ui/styles';
 import { Theme } from '@material-ui/core/styles';
-import {
-  Pageview as PageViewIcon,
-  Edit as EditIcon,
-  Delete as DeleteIcon,
-} from '@material-ui/icons';
-import { DataGrid, GridCellParams, GridColDef } from '@material-ui/data-grid';
 import { Rows } from './testData';
 import { ViewEmployeesPageText } from '../../../text';
 import { useHistory } from 'react-router';
 import { Add as AddIcon } from '@material-ui/icons';
 import BreadCrumb from '../../../modules/components/BreadCrumb';
+import Table from './Table';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -24,7 +19,6 @@ const useStyles = makeStyles((theme: Theme) =>
       width: 'inherit',
     },
     fullParentContainer: {
-      height: '100%',
       width: '100%',
     },
     card: {
@@ -33,89 +27,12 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
-const renderDetailsButton = (id: number | string) => {
-  const history = useHistory();
-  return (
-    <Grid container justifyContent="center" wrap="nowrap">
-      <Grid item xs>
-        <IconButton
-          onClick={() => history.push(`/employees/viewOne/${id}`)}
-          size="large"
-        >
-          <PageViewIcon />
-        </IconButton>
-      </Grid>
-      <Grid item xs>
-        <IconButton
-          onClick={() => history.push(`/employees/edit/${id}`)}
-          size="large"
-        >
-          <EditIcon />
-        </IconButton>
-      </Grid>
-      <Grid item xs>
-        <IconButton size="large">
-          <DeleteIcon />
-        </IconButton>
-      </Grid>
-    </Grid>
-  );
-};
-
-const columns: GridColDef[] = [
-  { field: 'lastName', headerName: 'Last name' },
-  { field: 'firstName', headerName: 'First name' },
-  { field: 'email', headerName: 'Email' },
-  { field: 'mobile', headerName: 'Mobile' },
-  { field: 'role', headerName: 'Role' },
-  { field: 'office', headerName: 'Office' },
-  { field: 'department', headerName: 'Department' },
-  {
-    field: '',
-    headerName: 'Actions',
-    sortable: false,
-    // disableClickEventBubbling: true,
-    renderCell: (params: GridCellParams) => renderDetailsButton(params.id),
-  },
-];
-
-const flexColumn = () => {
-  return columns.map((column) => {
-    const newObject = { ...column };
-    return Object.defineProperty(newObject, 'flex', {
-      value: 1,
-      configurable: true,
-      enumerable: true,
-    });
-  });
-};
-
-const fixedWidthColumn = () => {
-  return columns.map((column) => {
-    const newObject = { ...column };
-    if (column.field !== '')
-      return Object.defineProperty(newObject, 'width', {
-        value: 120,
-        configurable: true,
-        enumerable: true,
-      });
-    else
-      return Object.defineProperty(newObject, 'width', {
-        value: 150,
-        configurable: true,
-        enumerable: true,
-      });
-  });
-};
-
 interface ViewAllPresentationProp {
   rowsData: Rows[];
-  isAboveMinWidth: boolean;
 }
 
 export const ViewAllPresentation: React.FC<ViewAllPresentationProp> = ({
   rowsData,
-  isAboveMinWidth,
 }) => {
   const classes = useStyles();
   const history = useHistory();
@@ -150,14 +67,9 @@ export const ViewAllPresentation: React.FC<ViewAllPresentationProp> = ({
         </Grid>
         <Grid item xs className={classes.inheritHeight}>
           <Grid container direction="column" className={classes.inheritHeight}>
-            <Grid item container xs wrap="nowrap">
+            <Grid item container xs>
               <Grid item xs>
-                <DataGrid
-                  key="data-grid"
-                  rows={rowsData}
-                  columns={isAboveMinWidth ? flexColumn() : fixedWidthColumn()}
-                  autoPageSize
-                />
+                <Table rowsData={rowsData} />
               </Grid>
             </Grid>
           </Grid>
