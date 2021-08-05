264de8f203758f8035181e2d3043a22c65da986e
Tesfa Hailu <>
2021-08-05 15:30:01 -0400

    update: "view role" header and body

--- client/src/pages/Roles/ViewOne/ViewOnePresentation.tsx
+++ client/src/pages/Roles/ViewOne/ViewOnePresentation.tsx
@@ -1,79 +1,29 @@
-import { Grid, Typography, Button, Card, CardContent } from '@material-ui/core';
-import { createStyles, makeStyles } from '@material-ui/styles';
-import { Theme } from '@material-ui/core/styles';
 import React, { Fragment } from 'react';
-import { useHistory } from 'react-router';
-import { addSpaceAndUpperCaseText } from '../../../modules/utils/textRegex';
 import { ViewRolePageText } from '../../../text';
-import { RoleType } from '../../../types/types';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    textSpacingBelow: {
-      marginBottom: theme.spacing(0.5),
-    },
-    actionButtonSpacing: {
-      marginRight: theme.spacing(0.5),
-      marginBottom: theme.spacing(0.5),
-    },
-  }),
-);
+import { LimitedRoleType } from '../../../types/types';
+import { PageHeader } from '../../../modules/components/PageHeader';
+import { CardFormatted } from '../../../modules/components/CardFormatted';
 
 interface ViewOnePresentationProps {
-  role: RoleType;
+  role: LimitedRoleType;
 }
 
 export const ViewOnePresentation: React.FC<ViewOnePresentationProps> = ({
   role,
 }) => {
-  const classes = useStyles();
-  const history = useHistory();
   return (
     <Fragment>
-      <Grid container justifyContent="space-between">
-        <Typography variant="h5" className={classes.textSpacingBelow}>
-          {ViewRolePageText.PageHeaderText}
-        </Typography>
-        <Button
-          className={classes.actionButtonSpacing}
-          onClick={() => history.push(`/roles/edit/${role.id}`)}
-        >
-          {ViewRolePageText.EditButtonText}
-        </Button>
-      </Grid>
-      <Card>
-        <CardContent>
-          {/* {(Object.keys(role) as RoleType[]).map((key) => {
-            if (key === 'id') return;
-            return (
-              <CardRow
-                index={addSpaceAndUpperCaseText(key)}
-                value={role[key]}
-              />
-            );
-          })} */}
-        </CardContent>
-      </Card>
+      <PageHeader
+        title={ViewRolePageText.PageHeaderText}
+        subtitle={ViewRolePageText.PageSubHeaderText}
+        isButton={false}
+      />
+      <CardFormatted
+        headerText={ViewRolePageText.RoleText}
+        buttonText={ViewRolePageText.EditButtonText}
+        onEditButtonClick={() => {}}
+        data={role}
+      />
     </Fragment>
-  );
-};
-
-const CardRow = ({
-  index: key,
-  value,
-}: {
-  index: string;
-  value: string | number | undefined;
-}) => {
-  const classes = useStyles();
-  return (
-    <div className={classes.textSpacingBelow}>
-      <Typography variant="subtitle2" gutterBottom>
-        {key}
-      </Typography>
-      <Typography variant="subtitle1" gutterBottom>
-        {value}
-      </Typography>
-    </div>
   );
 };
