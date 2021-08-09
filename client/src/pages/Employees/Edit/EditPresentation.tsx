bf086d6d53b3127cc68f3d6ef0bb25d2fc5c8ff4
tesfahailu <40552713+tesfahailu@users.noreply.github.com>
2021-08-08 22:53:14 -0400

    update: edit employee work do include selection for office, deparment, role, and projects (#62)

--- client/src/pages/Employees/Edit/EditPresentation.tsx
+++ client/src/pages/Employees/Edit/EditPresentation.tsx
@@ -1,44 +1,46 @@
-import { Box, Button } from '@material-ui/core';
-import { createStyles, makeStyles } from '@material-ui/styles';
-import { Theme } from '@material-ui/core/styles';
-import React from 'react';
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Divider,
+  FormControl,
+  IconButton,
+  InputLabel,
+  List,
+  ListItem,
+  ListItemText,
+  Select,
+  Typography,
+} from '@material-ui/core';
+import { Delete as DeleteIcon } from '@material-ui/icons';
+import React, { Fragment } from 'react';
+import { PageHeader } from '../../../modules/components/PageHeader';
 import { EditEmployeePageText } from '../../../text';
 import { EditEmployeeType } from '../../../types/types';
-import { FormDepartment } from '../../../modules/components/FormDepartment';
 import { FormEmployeeAddress } from '../../../modules/components/FormEmployeeAddress';
 import { FormEmployee } from '../../../modules/components/FormEmployee';
-import { FormOffice } from '../../../modules/components/FormOffice';
-import { PageHeader } from '../../../modules/components/PageHeader';
-import { FormProjects } from '../../../modules/components/FormProjects';
-import { Fragment } from 'react';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    header: {
-      marginBottom: theme.spacing(2),
-    },
-    actionButtonSpacing: {
-      marginRight: theme.spacing(1),
-      marginBottom: theme.spacing(1),
-    },
-  }),
-);
 
 export const EditPresentation = ({
   employee,
   onEmployeeInfoChange,
   employeeAddress,
   onEmployeeAddressChange,
+  office,
+  onOfficeChange,
+  officesList,
   department,
   onDepartmentChange,
-  officeAddress,
-  onOfficeAddressChange,
+  departmentsList,
+  role,
+  onRoleChange,
+  rolesList,
   projects,
   onProjectChange,
+  projectsList,
   isFormChanged,
   saveChanges,
 }: EditEmployeeType) => {
-  const classes = useStyles();
   return (
     <Fragment>
       <PageHeader
@@ -46,7 +48,7 @@ export const EditPresentation = ({
         subtitle={EditEmployeePageText.PageSubHeaderText}
         isButton={false}
       />
-      <Box sx={{ mt: 2 }}>
+      <Box sx={{ mt: 1 }}>
         <FormEmployee
           employee={employee}
           onEmployeeInfoChange={onEmployeeInfoChange}
@@ -55,18 +57,104 @@ export const EditPresentation = ({
           address={employeeAddress}
           onAddressChange={onEmployeeAddressChange}
         />
-        <FormDepartment
-          department={department}
-          onDepartmentChange={onDepartmentChange}
-        />
-        <FormOffice
-          address={officeAddress}
-          onAddressChange={onOfficeAddressChange}
-        />
-        {/* <FormProjects projects={projects} onProjectChange={onProjectChange} /> */}
+        <Card sx={{ mb: 2 }}>
+          <CardContent>
+            <Typography variant="h6">Company:</Typography>
+            <Box sx={{ my: 2 }}>
+              <FormControl fullWidth>
+                <InputLabel htmlFor="outlined-office-native-simple">
+                  Office:
+                </InputLabel>
+                <Select
+                  native
+                  value={office.name}
+                  onChange={onOfficeChange('name')}
+                  label="Office"
+                  inputProps={{
+                    name: 'office',
+                    id: 'outlined-office-native-simple',
+                  }}
+                >
+                  {officesList.map(({ name }) => (
+                    <option value={name}>{name}</option>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+            <Box sx={{ mb: 2 }}>
+              <FormControl fullWidth>
+                <InputLabel htmlFor="outlined-department-native-simple">
+                  Department:
+                </InputLabel>
+                <Select
+                  native
+                  value={department.title}
+                  onChange={onDepartmentChange('title')}
+                  label="Department"
+                  inputProps={{
+                    name: 'department',
+                    id: 'outlined-department-native-simple',
+                  }}
+                >
+                  {departmentsList.map(({ name }) => (
+                    <option value={name}>{name}</option>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+            <Box sx={{ mb: 2 }}>
+              <FormControl fullWidth>
+                <InputLabel htmlFor="outlined-role-native-simple">
+                  Role:
+                </InputLabel>
+                <Select
+                  native
+                  value={role.name}
+                  onChange={onRoleChange('name')}
+                  label="Role"
+                  inputProps={{
+                    name: 'role',
+                    id: 'outlined-role-native-simple',
+                  }}
+                >
+                  {rolesList.map(({ name }) => (
+                    <option value={name}>{name}</option>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent>
+            <Box>
+              <Typography variant="h6"> Projects:</Typography>
+              <List dense={true}>
+                <Divider />
+                {projects.map(({ name, description }) => (
+                  <Fragment>
+                    <ListItem
+                      secondaryAction={
+                        <IconButton edge="end" aria-label="delete">
+                          <DeleteIcon />
+                        </IconButton>
+                      }
+                    >
+                      <ListItemText
+                        primary={name}
+                        secondary={description ? description : null}
+                      />
+                    </ListItem>
+                    <Divider />
+                  </Fragment>
+                ))}
+              </List>
+            </Box>
+          </CardContent>
+        </Card>
       </Box>
       <Button
-        className={classes.actionButtonSpacing}
+        sx={{ ml: 1, mt: 1 }}
         disabled={!isFormChanged}
         onClick={saveChanges}
       >
