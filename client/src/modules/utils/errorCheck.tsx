f5330ea3a1bd0c9ddb6d09aa7f82616d850f9caa
Tesfa Hailu <>
2021-08-18 22:57:08 -0400

    update: validation logic & move page text to separate file

---
+++ client/src/modules/utils/errorCheck.tsx
@@ -0,0 +1,30 @@
+export function minMaxLength(
+  text: string,
+  minLength: number,
+  maxLength?: number,
+) {
+  let result = !text || text.length < minLength;
+  if (maxLength) result = result || text.length < minLength;
+  return result;
+}
+
+export function validEmail(text: string) {
+  const regex = RegExp(
+    /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+  );
+  return !regex.test(text);
+}
+
+let registeredUsers = ['sfs@gmail.com'];
+
+export function userExists(username: string) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      if (registeredUsers.findIndex((u) => u === username) !== -1) {
+        resolve(true);
+      } else {
+        resolve(false);
+      }
+    });
+  });
+}
